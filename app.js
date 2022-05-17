842795a90f2eed689a0b7638b0dd73034d80b1a4
Okoye Victor <okoyeebuka64@gmail.com>
2022-05-17 23:13:32 +0100

    Added Error Handlers and Error Page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate'); 
+const handleAsync = require('./Utility/handleAsync');
+const ExpressError = require('./Utility/ExpressError');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Restaurant = require('./models/restaurant');
@@ -34,49 +36,60 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/restaurants', async (req, res) => {
+app.get('/restaurants', handleAsync( async (req, res) => {
     const restaurants = await Restaurant.find({})
     res.render('restaurants/index', { restaurants })
-})
+}))
 
 app.get('/restaurants/new', (req, res) => {
 
     res.render('restaurants/new')
 })
 
-app.post('/restaurants', async (req, res) => {
+app.post('/restaurants',handleAsync( async (req, res, next) => {
+    if(!req.body.campground) throw new ExpressError('Hey Invalid Data', 400)
     const restaurants = new Restaurant(req.body.restaurant);
     await restaurants.save();
     res.redirect(`/restaurants/${restaurants._id}`)
-})
+}))
 
-app.get('/restaurants/:id', async (req, res) => {
+app.get('/restaurants/:id',handleAsync( async (req, res) => {
     const { id } = req.params;
     const restaurants = await Restaurant.findById(req.params.id);
     res.render('restaurants/show', { restaurants })
-})
+}));
 
-app.get('/restaurants/:id/edit', async (req, res) => {
+app.get('/restaurants/:id/edit',handleAsync( async (req, res) => {
     const { id } = req.params;
     const restaurants = await Restaurant.findById(req.params.id);
 
     res.render('restaurants/edit', { restaurants });
-})
+}))
 
 
-app.put('/restaurants/:id', async (req, res) => {
+app.put('/restaurants/:id',handleAsync( async (req, res) => {
     const { id } = req.params;
     const restaurants = await Restaurant.findByIdAndUpdate(id, {
         ...req.body.restaurant
     })
     res.redirect(`/restaurants/${restaurants._id}`)
-})
+}))
 
-app.delete('/restaurants/:id', async (req, res) => {
+app.delete('/restaurants/:id',handleAsync( async (req, res) => {
     const { id } = req.params;
     const restaurants = await Restaurant.findByIdAndDelete(id);
     res.redirect('/restaurants');
-});
+}));
+
+app.all(('*'), (req,res,next)=>{
+    next(new ExpressError('This page does not exist', 404))
+})
+
+app.use((err,req,res,next)=>{
+    const {statusCode=500} = err;
+    if(!err.message) err.message = 'Something Went Wrong'
+    res.status(statusCode).render('error', {err})
+})
 
 app.listen(3000, () => {
     console.log("Listening ON Port 3000!!");
