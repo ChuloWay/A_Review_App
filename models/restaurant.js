df32362c7c7037a87f97900d857b588cdcf67c43
Okoye Victor <okoyeebuka64@gmail.com>
2022-06-07 21:24:35 +0100

    Added Review Model.

--- models/restaurant.js
+++ models/restaurant.js
@@ -8,7 +8,11 @@ const RestaurantSchema = new Schema({
     image:String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews:[{
+        type:Schema.Types.ObjectId,
+        ref:'Review'
+    }]
 });
 
 module.exports = mongoose.model('Restaurant', RestaurantSchema);
