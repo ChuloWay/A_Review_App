6d95ed3e3a8a15f9350b87c68f00488fc66ffe16
Okoye Victor <okoyeebuka64@gmail.com>
2022-06-28 23:14:46 +0100

    Styling Cluster Map

--- public/scripts/clusterMap.js
+++ public/scripts/clusterMap.js
@@ -5,7 +5,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'map', // container ID
-style: 'mapbox://styles/mapbox/dark-v10', // style URL
+style: 'mapbox://styles/mapbox/light-v10', // style URL
 zoom: 6, // starting zoom
 center: [-77.432, 25.0306] // starting position
 });
@@ -39,20 +39,20 @@ paint: {
 'circle-color': [
 'step',
 ['get', 'point_count'],
-'#51bbd6',
-100,
-'#f1f075',
-750,
-'#f28cb1'
+'#00BCD4',
+10,
+'#2196F3',
+30,
+'#3F51B5'
 ],
 'circle-radius': [
 'step',
 ['get', 'point_count'],
+15,
+10,
 20,
-100,
 30,
-750,
-40
+25
 ]
 }
 });
